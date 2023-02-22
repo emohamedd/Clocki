a91ba98a1a73229fd01b2d608e2bf9010bd57837
El gaouej Mohamed amine <a.elgaouej@gmail.com>
2023-02-22 23:48:31 +0100

    it clock

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-    let currentDate = new Date();
-    let hours = currentDate.getHours();
-    let minutes = currentDate.getMinutes();
-    let seconds = currentDate.getSeconds();
-    hours = hours < 10 ? '0' + hours : hours;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    console.log(hours + ':' + minutes + ':' + seconds);
+    setInterval(() => {
+        let currentDate = new Date();
+        let hours = currentDate.getHours();
+        let minutes = currentDate.getMinutes();
+        let seconds = currentDate.getSeconds();
+        let time = document.getElementById("time");
+        hours = hours < 10 ? '0' + hours : hours;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        time.textContent = hours + ':' + minutes + ':' + seconds;
+    }, 1000);
